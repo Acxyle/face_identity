1bd7a6a3661ce491fe433904f26b5263b462e988
Acxyle <pknacxyle200@gmail.com>
2023-08-21 21:52:00 +0100

    Update utils_.py

--- utils_.py
+++ utils_.py
@@ -8,6 +8,7 @@ Created on Wed Feb  8 15:12:39 2023
 import torch
 import torchvision.transforms as transforms
 
+import pickle
 import skimage
 import os
 import numpy as np
@@ -18,7 +19,153 @@ from spikingjelly.activation_based.model.tv_ref_classify import utils
 from spikingjelly.activation_based import functional
 
 from sklearn import svm
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+
+# ------ sigstar functions
+def sigstar(groups, stats, ax, nosort=False):
+    
+    if stats is None or len(stats) == 0:
+        stats = [0.05] * len(groups)
+        
+    xlocs = np.array(groups)
+    
+    """
+        Optionally sort sig bars from shortest to longest so we plot the shorter ones first
+        in the loop below. Usually this will result in the neatest plot. If we waned to 
+        optimise the order the sig bars are plotted to produce the neatest plot, then this 
+        is where we'd do it. Not really worth the effort, though, as few plots are complicated
+        enough to need this and the user can define the order very easily at the command line. 
+    """
+    if not nosort:
+        sort_inds = np.argsort(xlocs[:, 1] - xlocs[:, 0])
+        xlocs = xlocs[sort_inds]
+        stats = np.array(stats)[sort_inds]
+    
+    """
+        Add the sig bar lines and asterisks 
+    """
+    H = []
+    for i in range(len(groups)):
+        y_range = ax.get_ylim()[1] - ax.get_ylim()[0]
+        y_offset = y_range * 0.05
+        
+        thisY = findMinY(xlocs[i], ax) + y_offset
+        
+        h = makeSignificanceBar(xlocs[i], thisY, stats[i], ax)
+        H.append(h)
+        
+    """
+        Now we can add the little downward ticks on the ends of each line. We are
+        being extra cautious and leaving this it to the end just in case the y limits
+        of the graph have changed as we add the highlights. The ticks are set as a
+        proportion of the y axis range and we want them all to be the same the same
+        for all bars.
+    """
+    y_range = ax.get_ylim()[1] - ax.get_ylim()[0]
+    yd = y_range * 0.01
+    
+    for h in H:
+        line = h['line']
+        y = line.get_ydata()
+        y[0] -= yd
+        y[3] -= yd
+        line.set_ydata(y)
+
+    return H
+
+def makeSignificanceBar(x, y, p, ax):
+    """
+    makeSignificanceBar produces the bar and defines how many asterisks we get for a 
+    given p-value
+    """
+    if p<=1E-3:
+        stars='★★★'
+    elif p<=1E-2:
+        stars='★★'
+    elif p<=0.05:
+        stars='★'
+    elif np.isnan(p):
+        stars='N/A'
+    else:
+        stars='n.s.'
+        
+    x = np.tile(x, (2,1))
+    y = np.tile(y, (4,1))
+    
+    # [notice] need to find the correct way to retrieve this line
+    line, = ax.plot(x.T.ravel(), y, 'C0', linewidth=1.5)
+    line.set_label('sigstar_bar')
+    
+    if not np.isnan(p):
+        offset=0.015
+    else:
+        offset=0.02
+    
+    starY=np.mean(y)+myRange(ax.get_ylim())*offset
+    
+    text = ax.text(np.mean(x), starY, stars, horizontalalignment='center', backgroundcolor='none')
+    text.set_label('sigstar_stars')
+    
+    Y = ax.get_ylim()
+    if Y[1]<starY:
+        ax.set_ylim([Y[0], starY+myRange(Y)*0.05])
+            
+    H = {'line': line, 'text': text}
+    
+    return H
+    
+def myRange(x):
+    return np.max(x)-np.min(x)
+
+def findMinY(x, ax):
+    """
+    The significance bar needs to be plotted a reasonable distance above all the data points
+    found over a particular range of X values. So we need to find these data and calculat the 
+    the minimum y value needed to clear all the plotted data present over this given range of 
+    x values. 
+   
+    This version of the function is a fix from Evan Remington
+    """
+    
+    old_xlim = ax.get_xlim()
+    old_ylim = ax.get_ylim()
+    
+    ax.autoscale_view(tight=True)
+    
+    x[0] += 0.1
+    x[1] += 0.1
+    
+    ax.set_xlim(x)
+    
+    yLim = ax.get_ylim()
+    Y = max(yLim)
+    
+    #ax.relim()
+    #ax.autoscale_view()
+    #ax.set_aspect('auto')
+    
+    ax.set_xlim(old_xlim)
+    ax.set_ylim(old_ylim)
+    
+    return Y     # [question] should return ax
+
+def pickle_dump(file_path, file):
+    if not os.path.exists(file_path):
+        with open(file_path, 'wb') as f:
+            pickle.dump(file, f, protocol=-1)
+        f.close()
+    else:
+        raise RuntimeWarning('[Codwarning] file already exists, continue will add extra data into that file')
+
+def pickle_load(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as f:
+            loaded_file = pickle.load(f)
+        f.close()
+        return loaded_file
+    else:
+        raise RuntimeError('[Coderror] can not find the file')
 
 def generate_vgg_layers(model, model_name, T=4):     # FIXME add T and other useful attributes
     
@@ -315,21 +462,38 @@ def cal_acc1_acc5(output, target):
     acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
     return acc1, acc5
     
-def SVM_classification(matrix, label):     # [notice] use default setting of SVM
+def SVM_classification(matrix, label):
     # [notice] test_size should be the same with finetune but not 100% necessary, random_state is a personal preference
-    matrix_train, matrix_test, label_train, label_test = train_test_split(matrix, label, test_size=0.2, random_state=6)    
+    matrix_train, matrix_test, label_train, label_test = train_test_split(matrix, label, test_size=0.2, random_state=6)
+    
+    '''
+    # [notice] default kernel='rbf', which is non-linear. 
+    
+    It should be noticed the performance of svm.SVC() can be highly sensitive 
+    to the choice of kernel, used what kernel depends on the data and the 
+    research problem, sounds like can use GridSearchCV from sklearn.model_selection
+    to perform a grid search for best combination for a model, but perhaps time
+    consuming and computation intensive.
+    '''
     clf = svm.SVC()     # .SVC() .LinearSVC() .NuSVC() ... 
+    
+    
     if matrix_train.shape[1] == 0:
       acc = 0.
     else:
       clf.fit(matrix_train, label_train)
+      
       predicted = clf.predict(matrix_test)
-      correct = 0
-      samples = len(label_test)
-      for i in range(samples):
-          if predicted[i] == label_test[i]:
-              correct += 1
-      acc = correct / samples
+      
+      #correct = 0
+      #samples = len(label_test)
+      #for i in range(samples):
+      #    if predicted[i] == label_test[i]:
+      #        correct += 1
+      #acc = correct / samples
+      
+      acc = accuracy_score(label_test, predicted)
+      
     return acc
 
 def makeLabels(sample_num, class_num):  # generate a label list

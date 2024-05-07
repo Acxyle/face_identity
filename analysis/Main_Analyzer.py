4dfd9de2e7e7c07e572cdd7ec2a5817359938068
Acxyle <pknacxyle200@gmail.com>
2024-05-07 15:59:56 +0100

    add folds merge

--- analysis/Main_Analyzer.py
+++ analysis/Main_Analyzer.py
@@ -13,14 +13,12 @@ import os
 import time
 import argparse
 
-from Bio_Cell_Records_Process import Human_Neuron_Records_Process, Monkey_Neuron_Records_Process
+from spikingjelly import visualizing
 
 import FSA_ANOVA, FSA_Encode, FSA_DRG, FSA_RSA, FSA_CKA
-import Selectivity_Analysis_Feature
+#import Selectivity_Analysis_Feature
 
 import utils_
-
-from spikingjelly import visualizing
 
 
 # ======================================================================================================================
@@ -33,32 +31,86 @@ def get_args_parser():
     
     parser.add_argument("--FSA_root", type=str, default="/home/acxyle-workstation/Downloads/FSA", help="[Codelp] root directory for features and neurons")
     
-    parser.add_argument("--FSA_dir", type=str, default='VGG/SpikingVGG')
-    parser.add_argument("--FSA_config", type=str, default='SpikingVGG16bn_IF_ATan_T8_C2k_fold_')
+    parser.add_argument("--FSA_dir", type=str, default='Resnet/SEWResnet')
+    parser.add_argument("--FSA_config", type=str, default='SEWResnet152_IF_ATan_T4_C2k_fold_')
     parser.add_argument("--fold_idx", type=int, default=1)
 
-    parser.add_argument("--model", type=str, default='spiking_vgg16_bn')     
+    parser.add_argument("--model", type=str, default='sew_resnet152')     
     
     return parser.parse_args()
     
 
 # ----------------------------------------------------------------------------------------------------------------------
 # FIXME - under construction...
-class Multi_Model_Analysis(Monkey_Neuron_Records_Process, Human_Neuron_Records_Process):
+class Multi_Model_Analysis():
     
-    def __init__(self, feature_root_general='Face Identity VGG16bn_fold_', num_fold=5, **kwargs):
+    def __init__(self, args, num_folds=5, **kwargs):
 
-        self.feature_root_general = feature_root_general
-        self.num_fold = num_fold
-        self.model_structure = feature_root_general.split('_')[0].split(' ')[-1]
+        self.num_folds = num_folds
+        self.root = os.path.join(args.FSA_root, args.FSA_dir, f'FSA {args.FSA_config}')
         
-        self.model_root = os.path.join(args.FSA_root, self.feature_root_general)
-        utils_.make_dir(self.model_root)
+        self.model_structure = args.FSA_config.replace('C2k_fold_', '')
         
-        self.layers, self.neurons, _ = utils_.get_layers_and_units(model_name=args.model, feature_shape=(3,224,224))
-        _, self.layers, self.neurons, _ = utils_.activation_function(args.model, self.layers, self.neurons, act_only=True)
+        _, self.layers, self.neurons, self.shapes = utils_.get_layers_and_units(args.model, 'act')
         
-
+        self.used_unit_types = ['selective', 'strong_selective', 'weak_selective', 'non_selective']
+        
+        self._configs = {
+            'num_folds': self.num_folds,
+            'root': self.root,
+            'layers': self.layers,
+            'neurons': self.neurons
+            }
+        
+    def ANOVA_folds(self, **kwargs):
+    
+        FSA_ANOVA.FSA_ANOVA_folds(**self._configs)()
+        
+        
+    def Encode_folds(self, **kwargs):
+        
+        FSA_Encode_folds = FSA_Encode.FSA_Encode_folds(**self._configs)
+        FSA_Encode_folds(**kwargs)
+        FSA_Encode_folds(used_unit_types=self.used_unit_types, **kwargs)
+        
+    
+    def RSA_folds(self, **kwargs):
+        
+        #first_corrs = ['euclidean', 'pearson', 'spearman', 'mahalanobis', 'concordance']
+        first_corrs = ['euclidean', 'pearson', 'spearman', 'mahalanobis']
+        second_corrs = ['pearson', 'spearman', 'concordance']
+        
+        #RSA_unit_types = self.used_unit_types
+        RSA_unit_types = ['qualified', 'selective', 'non_selective', 'legacy']
+        
+        RSA_Monkey_folds = FSA_RSA.RSA_Monkey_folds(**self._configs)
+        for first_corr in first_corrs:
+            for second_corr in second_corrs:
+                RSA_Monkey_folds(first_corr=first_corr, second_corr=second_corr, route='p')
+        
+        RSA_Human_folds = FSA_RSA.RSA_Human_folds(**self._configs)
+        for first_corr in first_corrs:
+            for second_corr in second_corrs:
+                for used_unit_type in RSA_unit_types:
+                    for used_id_num in [50, 10]:
+                        RSA_Human_folds(first_corr=first_corr, second_corr=second_corr, used_unit_type=used_unit_type, used_id_num=used_id_num, route='p')
+        
+    
+    def CKA_folds(self, **kwargs):
+        
+        CKA_unit_types = ['qualified', 'selective', 'non_selective', 'legacy']
+        
+        # --- Monkey
+        FSA_CKA.CKA_Similarity_Monkey_folds(**self._configs)(kernel='linear')
+        #for threshold in [1.0, 10.0]:
+        #    CKA_Similarity_Monkey_folds(**self._configs)(kernel='rbf', threshold=threshold)
+        
+        # --- Human
+        for used_unit_type in CKA_unit_types:
+            FSA_CKA.CKA_Similarity_Human_folds(**self._configs)(kernel='linear', used_unit_type=used_unit_type, used_id_num=50)
+            #for threshold in [1.0, 10.0]:
+            #    CKA_Similarity_Human_folds(**self._configs)(kernel='rbf', threshold=threshold, used_unit_type=used_unit_type, used_id_num=50)
+        
 
 # ----------------------------------------------------------------------------------------------------------------------
 def single_model_analysis(args, thresholds=[1.0, 10.0]):
@@ -70,7 +122,10 @@ def single_model_analysis(args, thresholds=[1.0, 10.0]):
     # --- init
     config = f'{args.FSA_config}{args.fold_idx}'
     
-    FSA_folder = os.path.join(args.FSA_root, args.FSA_dir, f'FSA {args.FSA_config}/-_Single Models/FSA {config}')
+    if 'fold' in args.FSA_config:
+        FSA_folder = os.path.join(args.FSA_root, args.FSA_dir, f'FSA {args.FSA_config}/-_Single Models/FSA {config}')
+    else:
+        FSA_folder = os.path.join(args.FSA_root, args.FSA_dir, f'FSA {args.FSA_config}')
     
     _, layers, neurons, shapes = utils_.get_layers_and_units(args.model, 'act')
 
@@ -94,11 +149,12 @@ def single_model_analysis(args, thresholds=[1.0, 10.0]):
     Responses_analyzer.plot_stacked_responses(num_types=5)
     Responses_analyzer.plot_responses_PDF()
     Responses_analyzer.plot_pct_pie_chart()
+    Responses_analyzer.plot_Feature_Intensity(used_unit_types=['qualified', 'selective', 'strong_selective', 'weak_selective', 'non_selective'])
     del Responses_analyzer
     
-    SVM_analyzer = FSA_Encode.FSA_SVM(root=FSA_folder, layers=layers, neurons=neurons)
-    SVM_analyzer.process_SVM()
-    del SVM_analyzer
+    #SVM_analyzer = FSA_Encode.FSA_SVM(root=FSA_folder, layers=layers, neurons=neurons)
+    #SVM_analyzer.process_SVM()
+    #del SVM_analyzer
 
     # ----- 3. DR, DSM, Gram
     #DR_analyzer = FSA_DRG.FSA_DR(root=FSA_folder, layers=layers, neurons=neurons)
@@ -109,11 +165,12 @@ def single_model_analysis(args, thresholds=[1.0, 10.0]):
     #DSM_analyzer.process_DSM(metric='pearson')
     #del DSM_analyzer
     
-    #Gram_analyzer = FSA_DRG.FSA_Gram(root=FSA_folder, layers=layers, neurons=neurons)
-    #Gram_analyzer.calculation_Gram(kernel='linear', normalize=True)
+    Gram_analyzer = FSA_DRG.FSA_Gram(root=FSA_folder, layers=layers, neurons=neurons)
+    Gram_analyzer.calculation_Gram(kernel='linear', normalize=True)
+    Gram_analyzer.plot_Gram_intensity(kernel='linear')
     #for threshold in thresholds:
     #    Gram_analyzer.calculation_Gram(kernel='rbf', threshold=threshold)
-    #del Gram_analyzer
+    del Gram_analyzer
 
     # ----- 4. RSA
     #for monkey experiments
@@ -138,15 +195,15 @@ def single_model_analysis(args, thresholds=[1.0, 10.0]):
     
     # ----- 5. CKA
     CKA_monkey = FSA_CKA.CKA_Similarity_Monkey(root=FSA_folder, layers=layers, neurons=neurons)
-    CKA_monkey.process_CKA_monkey(kernel='linear', normalize=True)
-    for threshold in thresholds:
-        CKA_monkey.process_CKA_monkey(kernel='rbf', threshold=threshold, normalize=True)
+    CKA_monkey(kernel='linear', normalize=True)
+    #for threshold in thresholds:
+    #    CKA_monkey(kernel='rbf', threshold=threshold, normalize=True)
     
     CKA_human = FSA_CKA.CKA_Similarity_Human(root=FSA_folder, layers=layers, neurons=neurons)
     for used_unit_type in ['legacy', 'qualified', 'selective', 'non_selective']:
-        CKA_human.process_CKA_human(kernel='linear', used_unit_type=used_unit_type, normalize=True)
-        for threshold in thresholds:
-            CKA_human.process_CKA_human(kernel='rbf', threshold=threshold, used_unit_type=used_unit_type, normalize=True)
+        CKA_human(kernel='linear', used_unit_type=used_unit_type, normalize=True)
+        #for threshold in thresholds:
+        #    CKA_human(kernel='rbf', threshold=threshold, used_unit_type=used_unit_type, normalize=True)
     
     
     # ----- 6. Feature
@@ -173,12 +230,6 @@ if __name__ == "__main__":
     
     single_model_analysis(args)
     
-# =============================================================================
-#     multi_model_analysis = Multi_Model_Analysis(feature_root_general='Face Identity SpikingResnet18_IF_ATan_T16_CelebA2622_fold_')
-#     
-#     multi_model_analysis.plot_ANOVA_pct_multi_models()
-#     multi_model_analysis.plot_Encode_pct_multi_models()
-#     multi_model_analysis.plot_RSA_pct_multi_models()
-#     multi_model_analysis.plot_SVM_acc_multi_models()
-# =============================================================================
-
+    #multi_model_analysis = Multi_Model_Analysis(args)
+    #multi_model_analysis.CKA_folds()
+    
